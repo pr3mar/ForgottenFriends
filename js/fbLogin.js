32cbff18d5031e82883a7bd3f48703c5d4cb72bc
kikoA <kiko_skic@hotmail.com>
2015-05-16 23:28:38 +0200

    Button

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -2,16 +2,16 @@
  * Created by pr3mar on 16.05.2015.
  */
 // This is called with the results from from FB.getLoginStatus().
-$(document).ready(function() {
-    $.ajaxSetup({ cache: true });
-    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
-        FB.init({
-            appId: '456455124529286',
-            version: 'v2.3' // or v2.0, v2.1, v2.0
-        });
-        $('#loginbutton,#feedbutton').removeAttr('disabled');
-    });
-});
+//$(document).ready(function() {
+//    $.ajaxSetup({ cache: true });
+//    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+//        FB.init({
+//            appId: '456455124529286',
+//            version: 'v2.3' // or v2.0, v2.1, v2.0
+//        });
+//        $('#loginbutton,#feedbutton').removeAttr('disabled');
+//    });
+//});
 function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
