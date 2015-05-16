1689fbeb27a9400606345016ff2fba67172dc47b
pr3mar <n1A@s3A2>
2015-05-16 22:07:22 +0200

    fb login, test app

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 /**
  * Created by pr3mar on 16.05.2015.
  */
+$(document).ready(function() {
+    $.ajaxSetup({ cache: true });
+    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+        FB.init({
+            appId: '456455124529286',
+            version: 'v2.3' // or v2.0, v2.1, v2.0
+        });
+        $('#loginbutton,#feedbutton').removeAttr('disabled');
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+    });
+});
