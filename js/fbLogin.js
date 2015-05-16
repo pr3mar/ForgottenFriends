17fb162fafa3f15b608a80ed51eafaa644cb9458
pr3mar <n1A@s3A2>
2015-05-17 00:24:37 +0200

    calculate call

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -75,7 +75,7 @@ function initializeData() {
 
 $("#calculate").click(function(){
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me/feed', function(response) {
+    FB.api('/me/posts', function(response) {
         console.log(response.id);
     })
 });
