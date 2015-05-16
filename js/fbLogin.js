983a23f24825865f6213ce412d838c43a6f69ed1
pr3mar <n1A@s3A2>
2015-05-17 00:14:24 +0200

    calculate call

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -44,19 +44,8 @@ function statusChangeCallback(response) {
 // code below.
 function checkLoginState(func) {
     FB.getLoginStatus(function(response, func) {
-        if (response.status === 'connected') {
-            // Logged into your app and Facebook.
-            //initializeData();
+        if(statusChangeCallback()) {
             func();
-        } else if (response.status === 'not_authorized') {
-            // The person is logged into Facebook, but not your app.
-            document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
-        } else {
-            // The person is not logged into Facebook, so we're not sure if
-            // they are logged into this app or not.
-            document.getElementById('status').innerHTML = 'Please log ' +
-            'into Facebook.';
         }
     });
 }
