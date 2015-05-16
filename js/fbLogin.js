6a1f9d93d7fa67734b31aa789b9202a8991f8619
NaumGj <naum_gj@hotmail.com>
2015-05-16 23:33:02 +0200

    2 logins

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -37,11 +37,11 @@ function statusChangeCallback(response) {
 // This function is called when someone finishes with the Login
 // Button.  See the onlogin handler attached to it in the sample
 // code below.
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-}
+//function checkLoginState() {
+//    FB.getLoginStatus(function(response) {
+//        statusChangeCallback(response);
+//    });
+//}
 
 window.fbAsyncInit = function() {
     FB.init({
