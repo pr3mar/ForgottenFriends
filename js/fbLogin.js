fc24c908937df7c1c404a02c6a17129f46115756
NaumGj <naum_gj@hotmail.com>
2015-05-16 21:50:25 +0200

    Logged in

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -75,7 +75,7 @@ function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
         console.log('Successful login for: ' + response.name);
-        document.getElementById('status').innerHTML =
-            'Thanks for logging in, ' + response.name + '!';
+        document.getElementById('navbar').innerHTML =
+            '<p>Logged in!</p>';
     });
 }
