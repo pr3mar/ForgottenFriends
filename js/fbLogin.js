53f8bc8cda96fa674e7bf269a2bebd97f9ca4fad
pr3mar <n1A@s3A2>
2015-05-16 23:33:28 +0200

    2x login?

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -34,14 +34,14 @@ function statusChangeCallback(response) {
     }
 }
 
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-}
+//// This function is called when someone finishes with the Login
+//// Button.  See the onlogin handler attached to it in the sample
+//// code below.
+//function checkLoginState() {
+//    FB.getLoginStatus(function(response) {
+//        statusChangeCallback(response);
+//    });
+//}
 
 //window.fbAsyncInit = function() {
 //    FB.init({
