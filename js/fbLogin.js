2a6b9561c5f121c125a8322d43dbf29f8c35d1d2
pr3mar <n1A@s3A2>
2015-05-17 00:53:02 +0200

    calculate call

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -78,8 +78,10 @@ $("#calculate").click(function(){
     FB.api('/me/feed', function(response) {
         var ids = {};
         for(i = 0; i < response.length; i++) {
+            console.log(response[i])
             for (j = 0; j < response[i].likes.length; j++) {
                 var tmp = response[i].likes[j].id;
+                console.log(tmp);
                 if (tmp in ids) {
                     ids[tmp] += 1;
                 } else {
