460756faf288edc109f549249210a94e36205c53
pr3mar <n1A@s3A2>
2015-05-17 02:50:14 +0200

    calculate call

--- js/main.js
+++ js/main.js
@@ -40,6 +40,6 @@ $("#calculate").click(function(){
 function sortMapByValue(map) {
     var tupleArray = [];
     for (var key in map) tupleArray.push([key, map[key]]);
-    tupleArray.sort(function (a, b) { return a[1][0] > b[1][0] });
+    tupleArray.sort(function (a, b) { console.log(a,b); return a[1][0] > b[1][0] });
     return tupleArray;
 }
