8b588bf7521e92c87497518c7c5a68eb8e0d67bf
NaumGj <naum_gj@hotmail.com>
2015-05-17 11:38:10 +0200

    print

--- js/main.js
+++ js/main.js
@@ -102,12 +102,17 @@ function printMoreThanWeek(data, criticalIndex) {
     console.log(criticalIndex);
     document.getElementById("forgotten").innerHTML =
         '<table class="table table-striped">' +
-            '<thead><tr><td>Name</td><td>FB Link</td></tr></thead>' +
-            '<tbody><tr><th><a href="https://www.facebook.com/'+ data[criticalIndex][2] + '">' + data[criticalIndex][0] + '</a></th></tr>' +
-            '<tr><th><a href="https://www.facebook.com/'+ data[criticalIndex - 1][2] + '">' + data[criticalIndex-1][0] +'</a></th></tr>' +
-            '<tr><th><a href="https://www.facebook.com/'+ data[criticalIndex - 2][2] + '">' + data[criticalIndex-2][0] +'</a></th></tr>' +
-            '<tr><th><a href="https://www.facebook.com/'+ data[criticalIndex - 3][2] + '">' + data[criticalIndex-3][0] +'</a></th></tr>' +
-            '<tr><th><a href="https://www.facebook.com/'+ data[criticalIndex - 4][2] + '">' + data[criticalIndex-4][0] +'</a></th></tr>' +
+            '<thead><tr><td>Forgotten friends</td><td>Unfrequent friends</td></tr></thead>' +
+            '<tbody><tr><th><a href="https://www.facebook.com/'+ data[criticalIndex][2] + '">' + data[criticalIndex][0] + '</a></th>' +
+        '<th><a href="https://www.facebook.com/'+ data[data.length - 1][2] + '">' + data[data.length - 1][0] + '</a></th></tr>' +
+            '<tr><th><a href="https://www.facebook.com/'+ data[criticalIndex - 1][2] + '">' + data[criticalIndex-1][0] +'</a></th>' +
+        '</tr>' +
+            '<tr><th><a href="https://www.facebook.com/'+ data[criticalIndex - 2][2] + '">' + data[criticalIndex-2][0] +'</a></th>' +
+        '</tr>' +
+            '<tr><th><a href="https://www.facebook.com/'+ data[criticalIndex - 3][2] + '">' + data[criticalIndex-3][0] +'</a></th>' +
+        '</tr>' +
+            '<tr><th><a href="https://www.facebook.com/'+ data[criticalIndex - 4][2] + '">' + data[criticalIndex-4][0] +'</a></th>' +
+        '</tr>' +
         '</tbody></table>'; //'</th><th>https://www.facebook.com/' + data[criticalIndex-1][2] + '</th></tr></tbody></table>';
     /*for(i = criticalIndex; i > (criticalIndex - 5); i--){
         document.getElementById("tabela-forgotten").innerHTML = '<tr><th>' + data[i] + '</th></tr>'
